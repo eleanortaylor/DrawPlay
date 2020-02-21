ae42388027188ea0f706e2e093871981a8212a1c
eleanortaylor <eleanortaylor@mymail.mines.edu>
2020-02-20 21:29:28 -0700

    Changed the text, see <https://www.youtube.com/watch?v=Dt4zvJNXbdI>

--- src/Cat.java
+++ src/Cat.java
@@ -41,9 +41,12 @@ public class Cat {
 		x = catX + MOUTH_X;
 		y = catY + MOUTH_Y;
 		g2.fillOval(x, y, MOUTH_WIDTH, MOUTH_HEIGHT);
+		
+		// Add text!
 		g2.setColor(Color.black);
-		// Meow text appears below cat head, +10 places below 
-		// so it doesn't overlap the drawing
-		g2.drawString("Meow", catX, catY+HEAD_DIMENSION+10);	
+		g2.drawString("cat!", catX, catY+HEAD_DIMENSION+10);
+		g2.drawString("I'm a kitty cat", catX, catY+HEAD_DIMENSION+30);
+		g2.drawString("and I dance dance dance", catX+40, catY+HEAD_DIMENSION+50);
+		g2.drawString("and I dance dance dance!!", catX+50, catY+HEAD_DIMENSION+60);
 	}
 }
